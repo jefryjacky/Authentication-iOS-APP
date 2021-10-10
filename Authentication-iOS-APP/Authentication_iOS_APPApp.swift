1056c570bcd60fc5396d1c7526898e3c5558e80c
Jefry Jacky <jefryjacky@gmail.com>
2021-10-10 16:30:11 +0700

    Initial Commit

---
+++ Authentication-iOS-APP/Authentication_iOS_APPApp.swift
@@ -0,0 +1,20 @@
+//
+//  Authentication_iOS_APPApp.swift
+//  Authentication-iOS-APP
+//
+//  Created by Jefry Jacky on 10/10/21.
+//
+
+import SwiftUI
+
+@main
+struct Authentication_iOS_APPApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
